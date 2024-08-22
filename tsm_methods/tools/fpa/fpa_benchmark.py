09440496dc427cd7c3fd22591287966e747330a2
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-08-22 11:07:38 +0300

    Test.

--- tsm_methods/tools/fpa/fpa_benchmark.py
+++ tsm_methods/tools/fpa/fpa_benchmark.py
@@ -22,8 +22,9 @@ for line in lines:
 lines2 = []
 for line in lines:
     inversion_length = random.randrange(15, 25)
-    offset = random.randrange(length // 4, length // 4 * 3 - inversion_length)
+    offset = random.randrange(length // 2 - inversion_length, length // 2 + inversion_length)
     line2 = line[:offset] + line[offset:offset+inversion_length][::-1] + line[offset+inversion_length:]
+    assert len(line) == len(line2)
     lines2.append(line2)
 
 start_time = datetime.now()
@@ -34,8 +35,11 @@ f.set_int("scan_flank",100)
 f.set_int("scan_flank",60)
 
 for [line1, line2] in zip(lines, lines2):
+#for [line1, line2] in zip(lines[:-1], lines[1:]):
     hits1 = f.scan_two(line1, line2, False)
     hits2 = f.scan_two(line2, line1, False)
+    #print(hits1)
+    #print(hits2)
 
 end_time = datetime.now()
 
