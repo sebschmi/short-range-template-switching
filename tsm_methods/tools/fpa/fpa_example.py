b8205b06eea1c0f8cf097950cb3273dbe773f050
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-08-22 10:16:22 +0300

    Fix fpa import.

--- tsm_methods/tools/fpa/fpa_example.py
+++ tsm_methods/tools/fpa/fpa_example.py
@@ -1,6 +1,9 @@
-#/usr/bin/env python3
+print("Hello")
 
 import sys
 
-sys.path.insert(0, '.')
+sys.path.insert(0, '/home/sebschmi/git/short-range-template-switching/tsm_methods/tools/fpa')
+print(sys.path)
 import fpa_ext2
+
+print('Goodbye')
