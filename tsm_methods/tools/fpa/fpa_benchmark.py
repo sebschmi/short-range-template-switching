37f2cf3058e70e026ebc87777f374971ac137b67
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-08-22 11:02:25 +0300

    Benchmark identical strings with inversions.

--- tsm_methods/tools/fpa/fpa_benchmark.py
+++ tsm_methods/tools/fpa/fpa_benchmark.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import sys
+import random
 
 sys.path.insert(0, '/home/sebschmi/git/short-range-template-switching/tsm_methods/tools/fpa')
 import fpa_ext2
@@ -18,6 +19,12 @@ with open('sequences.txt') as file:
 for line in lines:
     assert len(line) == length, "sequence not long enough"
 
+lines2 = []
+for line in lines:
+    inversion_length = random.randrange(15, 25)
+    offset = random.randrange(length // 4, length // 4 * 3 - inversion_length)
+    line2 = line[:offset] + line[offset:offset+inversion_length][::-1] + line[offset+inversion_length:]
+
 start_time = datetime.now()
 
 f = fpa_ext2.FPA2()
@@ -25,10 +32,10 @@ f.set_int("min_length",8)
 f.set_int("scan_flank",100)
 f.set_int("scan_flank",60)
 
-for [line1, line2] in zip(lines[1:], lines[:-1]):
+for [line1, line2] in zip(lines, lines2):
     hits1 = f.scan_two(line1, line2, False)
     hits2 = f.scan_two(line2, line1, False)
 
 end_time = datetime.now()
 
-print(f"Computing {len(lines) - 1} alignments of length {length} took {(end_time - start_time).total_seconds()} seconds")
+print(f"Computing {len(lines)} alignments of length {length} took {(end_time - start_time).total_seconds()} seconds")
